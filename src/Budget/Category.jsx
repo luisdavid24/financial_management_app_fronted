fe987d241035a693cb95f6acb00d41d4197975d7
Luis David <david12.29@hotmail.com>
2025-02-12 17:37:23 -0500

    Category has some changes

--- src/Budget/Category.jsx
+++ src/Budget/Category.jsx
@@ -5,12 +5,6 @@ export default function Category({ name }) {
   return (
     <div className="budget-box">
       <h2>{name}</h2>
-      <p className="amount">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ut
-        nostrum cum corporis nesciunt praesentium fuga id magnam illum, aut
-        architecto quo, fugit ipsum, numquam error voluptas harum? Voluptas,
-        eligendi!
-      </p>
     </div>
   );
 }
